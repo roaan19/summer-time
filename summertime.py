c0edf3565f6b96fb895c4c6998852fe0ad48af41
roaan19 <141211644+roaan19@users.noreply.github.com>
2025-04-02 11:38:55 +0530

    Add files via upload

---
+++ summertime.py
@@ -0,0 +1,8 @@
+temperature=float(input("enter temperature"))
+print("your should wear")
+
+if temperature >=1 :
+    print("wear light clothes ")
+
+elif temperature <=1:
+    print("wear heavy clothes")
